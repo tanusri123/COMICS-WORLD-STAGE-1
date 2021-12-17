243ed5c0922a3bb3409aef8741e7632dc9fa5118
tanusri123 <67907113+tanusri123@users.noreply.github.com>
2021-12-17 19:07:46 +0530

    Add files via upload

--- Poster.js
+++ Poster.js
@@ -1,6 +1,8 @@
 AFRAME.registerComponent("poster", {
   init: function () {
     this.createPoster();
+    this.handleMouseEnterEvent();
+    this.handleMouseLeaveEvent();
   },
 
 createPoster:function(){
@@ -14,5 +16,24 @@ createPoster:function(){
 
   entityEl.setAttribute("position",{x:0, y:5, z:0.1});
   entityEl.setAttribute("material",{ src: item.url});
-} ;
+},
+
+handleMouseEnterEvent:function(){
+  this.el.addEventListner("mouseenter",()=>{
+    const id = this.el.getAttribute("id");
+    const posterId =[
+      "superman",
+      "spiderman",
+      "captain-aero",
+      "outer-space",
+    ];
+    if(posterId.includes(id)){
+      const postersContainer = document.querySeector("#posters-container");
+      posterContainer.setAttribute("cursor-listner",{
+        selectedItemId: id,
+      });
+      this.el.setAttriute("material",{color:"#1565c0"})
+    }
+  });
+}
 };
